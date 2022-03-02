b929c1381e1922acab13bd7a6b80fb70a1419143
hendridg <hendridg@gmail.com>
2022-03-02 15:08:29 -0400

    Add Missions Rockets and Profile components files

---
+++ src/components/Missions.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Missions() {
+  return <div>Missions</div>;
+}
+
+export default Missions;
---
+++ src/components/Rockets.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Rockets() {
+  return <div>Rockets</div>;
+}
+
+export default Rockets;
