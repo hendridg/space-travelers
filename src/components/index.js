a851ad71f7c255cbeee7f7cbdf0981e26fafe118
hendridg <hendridg@gmail.com>
2022-03-02 14:55:43 -0400

    Add index.js file to components folder to export

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Header from './Header';
+import Missions from './Missions';
+import Rockets from './Rockets';
+import Profile from './Profile';
+
+// prettier-ignore
+export {
+  Header, Missions, Rockets, Profile 
+};
