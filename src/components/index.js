53b0fe678dcb185175ca6684b903c2b967de9125
hendridg <hendridg@gmail.com>
2022-03-04 10:21:25 -0400

    Add export to Dragons.js components/index.js file

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,9 @@ import Header from './Header';
 import Missions from './Missions';
 import Rockets from './Rockets';
 import Profile from './Profile';
+import Dragons from './Dragons';
 
 // prettier-ignore
 export {
-  Header, Missions, Rockets, Profile,
+  Header, Missions, Rockets, Profile, Dragons,
 };
